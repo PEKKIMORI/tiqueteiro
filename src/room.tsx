2d73bf154022bab4968db9ca5595324b369e63fe
PEKKI MORI <pacrafa0806@gmail.com>
2023-05-26 19:36:32 -0300

    room tickets added

--- src/room.tsx
+++ src/room.tsx
@@ -2,8 +2,34 @@ import './room.css'
 
 export default function Room (){
     return(
-        <div>
-             <h1>Hello my friendo</h1>
+        <>
+        <h1 className="fonfon">Compre já seus ingressos!</h1>
+        <h3 style={{textAlign: 'center'}}>:D</h3>
+
+        <div className= "box">
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <p className="title">INGRESSO 1</p>
+                            <img src= "https://cdn.discordapp.com/attachments/885280158704074884/1111762811354366003/Movie-Ticket-PNG.png"/>
+                        </div>
+                        <div className="flip-card-back">
+                            <button className="title">COMPRAR</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <p className="title">INGRESSO 2</p>
+                            <img src= "https://cdn.discordapp.com/attachments/885280158704074884/1111762811354366003/Movie-Ticket-PNG.png"/>
+                        </div>
+                        <div className="flip-card-back">
+                            <button className="title">COMPRAR</button>
+                        </div>
+                    </div>
+                </div>
         </div>
+        </>
     )
 }
