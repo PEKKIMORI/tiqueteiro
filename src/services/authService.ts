88449d3d31c2741e7229cc54c140a00ccedb80e9
c0ffex <yudisakuma@gmail.com>
2023-06-04 13:54:29 -0300

    arroz

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -50,12 +50,12 @@ const authService = {
   return res
   },
   payment: async (ticket: string) => {
-    const res = await api.post(`/payment/${ticket}`).catch((error) => {
+    const res = await api.post(`/payment/eee90279-7bb9-410b-94bc-bc645a765333`).catch((error) => {
       if (error.response.status === 500 || error.response.status === 501) {
         return error.response;
       }
     })
-    
+  console.log(ticket)
   return res
   },
   getTickets:async (token: string) => {
