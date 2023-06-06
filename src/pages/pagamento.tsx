4d0aef24be8967fba4e330eeec8cac1a868d2cdc
c0ffex <yudisakuma@gmail.com>
2023-06-06 13:46:02 -0300

    aawa

--- src/pages/pagamento.tsx
+++ src/pages/pagamento.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from 'react';
 import { initMercadoPago, Payment, Wallet} from '@mercadopago/sdk-react';
-import { IBrickError } from '@mercadopago/sdk-react/bricks/util/types/common';
 
-import {  redirect, useParams } from 'react-router-dom';
+import {   useParams } from 'react-router-dom';
 import authService from '../services/authService';
 
 import '../css/pagamento.css'
-
 
 initMercadoPago('TEST-480b4324-8e4c-4565-a03b-07a158c89fdf');
 
@@ -46,13 +44,15 @@ function Pagamento() {
         <p className={"pog2"}>Faça o pagamento e contamos com a sua presença <span className={"pogspan"}>:)</span></p>
       <div className={"wadawel"}> Após finalizar o pagamento, você receberá seu ingresso pelo seu e-mail, então se certifique de colocá-lo corretamente, e não se esqueça de trazer o ingresso consigo no seu celular no dia da festa! </div>
       <div>
-      <div id="wallet_container">
-         <Wallet initialization={{ preferenceId: preferenceId }} />
-       </div>
-    </div>
+        {preferenceId && (
+          <div id="wallet_container">
+            <Wallet initialization={{ preferenceId: preferenceId }} />
+          </div>
+        )}
+      </div>
     </div>
     </>
   )
-  }
+}
 
 export default Pagamento;
