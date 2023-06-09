29c2a6e9d47f927f3b0525e5806ecb5979619c79
c0ffex <yudisakuma@gmail.com>
2023-06-09 14:09:33 -0300

    dasd

--- src/pages/validar.tsx
+++ src/pages/validar.tsx
@@ -1,37 +1,49 @@
-import { useState } from 'react';
-import authService from '../services/authService';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
 
 export default function TicketValidation() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [payerEmail, setPayerEmail] = useState<string>('');
-  const [payerName, setPayerName] = useState<string>('');
-  const [code, setCode] = useState<string>('');
-  const [validationResult, setValidationResult] = useState('');
+  const [payerEmail, setPayerEmail] = useState<string>("");
+  const [payerName, setPayerName] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+  const [validationResult, setValidationResult] = useState("");
+
+  useEffect(() => {
+    const checkUserRole = async () => {
+      const token = sessionStorage.getItem("onebitflix-token");
+      if (token) {
+        try {
+          const response = await authService.findUser(token);
+          if (response.data.role === "user") {
+            navigate("/room");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        navigate("/");
+      }
+    };
+
+    checkUserRole();
+  }, [navigate]);
 
   const handleValidation = async (event: React.FormEvent) => {
     event.preventDefault();
     const params = { payerName, payerEmail, code };
-    
-    try {
-      const token = sessionStorage.getItem('onebitflix-token');
-      const res = await authService.findUser(token!)
-      if(res.data.role === 'user') {
-        navigate('/room')
-      }
 
-      console.log(params)
-      const { data, status } = await authService.validate(params)
+    try {
+      const { data, status } = await authService.validate(params);
       if (status === 201) {
-        setValidationResult('O codigo foi validado');
+        setValidationResult("O código foi validado.");
       } else {
         alert(data.message);
       }
     } catch (error) {
       console.log(error);
     }
-    console.log(code)
   };
 
   return (
@@ -41,7 +53,7 @@ export default function TicketValidation() {
         <div>
           <label htmlFor="payerEmail">payerEmail:</label>
           <input
-            type="payerEmail"
+            type="email"
             id="payerEmail"
             value={payerEmail}
             onChange={(e) => setPayerEmail(e.target.value)}
