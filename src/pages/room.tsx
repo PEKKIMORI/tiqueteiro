ccb741e653fea064b774fc47f8ca7b422a25c174
PEKKI MORI <pacrafa0806@gmail.com>
2023-06-11 19:22:31 -0300

    l

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -64,15 +64,15 @@ export default function Room() {
   return (
     <>
     <div className="ragatanga">
-    <div className={"wadawel"}> Após finalizar o pagamento, você receberá seu ingresso pelo seu e-mail, então se certifique de colocá-lo corretamente, e não se esqueça de trazer o ingresso consigo no seu celular no dia da festa! </div>
-      <div className="box">
-        <div className="container">
+    <div className="container">
         <div className={"text-box"}>
           <h1 className={"pog1"}>FESTA JUNINA</h1>
           <h1 className={"pog1"}>FESTA JUNINA</h1>
         </div>
           <p className={"pog2"}>Faça o pagamento e contamos com a sua presença <span className={"pogspan"}>:)</span></p>
+          <div className={"wadawel"}> Após finalizar o pagamento, você receberá seu ingresso pelo seu e-mail, então se certifique de colocá-lo corretamente, e não se esqueça de trazer o ingresso consigo no seu celular no dia da festa! </div>
         </div>
+      <div className="box">
 
       {user?.role === 'admin' && (
       <div className="admin-buttons">
