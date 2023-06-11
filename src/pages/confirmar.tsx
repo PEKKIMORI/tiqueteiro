ab77762887326fc5787bdc322f6a6e588709fabe
PEKKI MORI <pacrafa0806@gmail.com>
2023-06-11 19:54:45 -0300

    ad

--- src/pages/confirmar.tsx
+++ src/pages/confirmar.tsx
@@ -1,7 +1,7 @@
 import  { useState } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-import '../css/room.css'
+import '../css/confirmar.css'
 
 const ConfirmEmail = () => {
   const navigate = useNavigate()
